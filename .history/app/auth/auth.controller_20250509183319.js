ae3e73f5c402fbf41271e15fcb404796b621dca0
gonov <adamej10@gmail.com>
2025-05-09 18:33:39 +0300

    12

--- .history/app/auth/auth.controller_20250509183319.js
+++ .history/app/auth/auth.controller_20250509183319.js
@@ -1,18 +1,18 @@
-import { faker } from '@faker-js/faker';
-import { hash, verify } from 'argon2';
-import asyncHandler from 'express-async-handler';
+import { faker } from '@faker-js/faker'
+import { hash, verify } from 'argon2'
+import asyncHandler from 'express-async-handler'
 
-import { prisma } from '../prisma.js';
-import { UserFields } from '../utils/user.utils.js';
+import { prisma } from '../prisma.js'
+import { UserFields } from '../utils/user.utils.js'
 
-import { generateToken } from './generate-token.js';
+import { generateToken } from './generate-token.js'
 
 // @desc    Auth user
 // @route   POST /api/auth/login
 // @access  Public
 export const authUser = asyncHandler(async (req, res) => {
   const login = req.body.login || req.body.username;
-  const { password } = req.body;
+  const password = req.body.password;
 
   if (!login || !password) {
     res.status(400);
@@ -20,7 +20,7 @@ export const authUser = asyncHandler(async (req, res) => {
   }
 
   const user = await prisma.user.findUnique({
-    where: { login },
+    where: { login }
   });
 
   if (!user) {
@@ -39,34 +39,35 @@ export const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+
 // @desc    Register user
 // @route   POST /api/auth/register
 // @access  Public
 export const registerUser = asyncHandler(async (req, res) => {
-  const { login, email, password } = req.body;
+	const { login, email, password } = req.body
 
-  const isHaveUser = await prisma.user.findUnique({
-    where: {
-      login,
-    },
-  });
+	const isHaveUser = await prisma.user.findUnique({
+		where: {
+			login
+		}
+	})
 
-  if (isHaveUser) {
-    res.status(400);
-    throw new Error('User already exists');
-  }
+	if (isHaveUser) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
 
-  const user = await prisma.user.create({
-    data: {
-      login,
-      email,
-      password: await hash(password),
-      name: faker.name.fullName(),
-    },
-    select: UserFields,
-  });
+	const user = await prisma.user.create({
+		data: {
+			login,
+			email,
+			password: await hash(password),
+			name: faker.name.fullName()
+		},
+		select: UserFields
+	})
 
-  const token = generateToken(user.id);
+	const token = generateToken(user.id)
 
-  res.json({ user, token });
-});
+	res.json({ user, token })
+})
